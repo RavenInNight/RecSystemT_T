c3b610a72a354806886e0781fab2737739dd13ed
Liza Karmanchuk <131619223+RavenInNight@users.noreply.github.com>
2025-06-02 22:14:58 +0300

    Delete MapPage directory

--- MapPage/Presenter/MapPresenterInput.swift
+++
@@ -1,6 +0,0 @@
-import CoreLocation
-
-protocol MapPresenterInput {
-    func viewDidLoad()
-    func didTapOnCoordinate(_ coordinate: CLLocationCoordinate2D)
-}
--- MapPage/View/MapViewInput.swift
+++
@@ -1,7 +0,0 @@
-import CoreLocation
-import MapKit
-
-protocol MapViewInput: AnyObject {
-    func setupInitialRegion(_ region: MKCoordinateRegion)
-    func addAnnotation(at coordinate: CLLocationCoordinate2D, title: String?)
-}
